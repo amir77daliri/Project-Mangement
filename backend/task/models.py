c849799159c2fa6749cf8bc8ffcebc10c9663f75
Amirhossein_Daliri <amirdalir0111@gmail.com>
2024-06-19 16:44:55 +0330

    initiate implemention of models

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from project.models import Project
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    )
+
+    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(choices=STATUS_CHOICES, default='pending')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"comment by {self.author} - {self.content[:20]}"
