922391901ef419849ad3157529d38afa8c9449f3
Amirhossein_Daliri <amirdalir0111@gmail.com>
2024-06-19 19:20:07 +0330

    Add Caching and cache invalidation for Task endpoints

--- backend/task/views.py
+++ backend/task/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import TaskSerializer, CommentSerializer
 from .models import Task, Comment
+from django.core.cache import cache
+
+
+Task_LIST_CACHE_KEY = 'task_list'
 
 
 class TaskListCreateApiView(generics.ListCreateAPIView):
@@ -12,8 +16,16 @@ class TaskListCreateApiView(generics.ListCreateAPIView):
         return Task.objects.all()
 
     def get(self, request, *args, **kwargs):
+        task_list = cache.get(Task_LIST_CACHE_KEY)
+        # check for cached data :
+        if task_list is not None:
+            print('get from cache...')
+            return Response(task_list, status=status.HTTP_200_OK)
+        # get from db and cache data
         queryset = self.get_queryset()
         data = self.serializer_class(queryset, many=True).data
+        cache.set(Task_LIST_CACHE_KEY, data)
+        print('get from db....')
         return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
@@ -21,6 +33,8 @@ class TaskListCreateApiView(generics.ListCreateAPIView):
         ser_data = self.serializer_class(data=data)
         ser_data.is_valid(raise_exception=True)
         ser_data.save()
+        # invalidate cache
+        cache.delete(Task_LIST_CACHE_KEY)
         return Response(ser_data.data, status=status.HTTP_201_CREATED)
 
 
@@ -28,12 +42,23 @@ class TaskRetrieveUpdateDestroyApiView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
+    def put(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        # invalidation cache :
+        cache.delete(Task_LIST_CACHE_KEY)
+        return response
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        # invalidation cache :
+        cache.delete(Task_LIST_CACHE_KEY)
+        return response
+
 
 class RetrieveTaskCommentsApiView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
 
     def get_queryset(self):
-        print('here...', self.kwargs)
         task_id = self.kwargs.get('pk')
         task = get_object_or_404(Task, pk=task_id)
         return Comment.objects.filter(task=task)
